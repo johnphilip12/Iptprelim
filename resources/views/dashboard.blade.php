e7e3a204baa09ee8227d55851e347ef3403d9d54
John Philip Villamor <villamorjohnphilip2@gmail.com>
2022-09-19 17:49:47 +0800

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+@extends('base')
+
+@extends('navbar')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <div class="container m-3">
+        <h1>Dashboard</h1>
+        <div class="card offset-sm-2 border border-light mt-5">
+            <div class="card-header">
+                <img class="img-responsive center-block d-block mx-auto rounded-circle" src="images/wiz.jpg" alt="logo">
+            </div>
+            
+            
+        </div>
+    </div>
+
+@endsection
